560da4ababfdc100494e787946e1844fa7c61b0e
Tiago Gomes <contato@tiagogomes.dev.br>
2025-07-06 20:53:22 -0300

    Finished the Board class

--- src/br/dev/tiagogomes/model/Board.java
+++ src/br/dev/tiagogomes/model/Board.java
@@ -1,6 +1,11 @@
 package br.dev.tiagogomes.model;
 
+import java.util.Collection;
 import java.util.List;
+
+import static br.dev.tiagogomes.model.GameStatusEnum.*;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class Board {
 	
@@ -14,5 +19,43 @@ public class Board {
 		return spaces;
 	}
 	
+	public GameStatusEnum getStatus () {
+		if (spaces.stream ().flatMap (Collection :: stream).noneMatch (s -> !s.isFixed () && nonNull (s.getActual ()))) {
+			return NON_STARTED;
+		}
+		return spaces.stream ().flatMap (Collection :: stream).anyMatch (s -> isNull (s.getActual ())) ? INCOMPLETE : COMPLETE;
+	}
 	
+	public boolean hasErrors () {
+		if (getStatus () == NON_STARTED) {
+			return false;
+		}
+		return spaces.stream ().flatMap (Collection :: stream).anyMatch (s -> nonNull (s.getActual ()) && !s.getActual ().equals (s.getExpected ()));
+	}
+	
+	public boolean chageValue (final int col, final int row, final int value) {
+		var space = spaces.get (col).get (row);
+		if (space.isFixed ()) {
+			return false;
+		}
+		space.setActual (value);
+		return true;
+	}
+	
+	public boolean clearValue (final int col, final int row) {
+		var space = spaces.get (col).get (row);
+		if (space.isFixed ()) {
+			return false;
+		}
+		space.clearSpace ();
+		return true;
+	}
+	
+	public void reset () {
+		spaces.forEach (c -> c.forEach (Space :: clearSpace));
+	}
+	
+	public boolean gameIsFinished(){
+		return !hasErrors () && getStatus ().equals (COMPLETE);
+	}
 }
---
+++ src/br/dev/tiagogomes/model/GameStatusEnum.java
@@ -0,0 +1,6 @@
+package br.dev.tiagogomes.model;
+
+public enum GameStatusEnum {
+	
+	NON_STARTED, INCOMPLETE, COMPLETE
+}
